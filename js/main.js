76917411f659aba019f08ebfe7c1c82c5d7dea33
Mijail C <mec_94@outlook.com.ar>
2022-03-05 17:24:28 -0300

    March 5

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-/* eslint-disable require-jsdoc */
+/* eslint-disable */
 
 
 // Ocultar o mostrar paneles no seleccionados 
 
-var hiddenPanel = document.getElementsByClassName('hiddenPanel');
+const hiddenPanel = document.getElementsByClassName('hiddenPanel');
 
 function showPanels(p) {
   if (p == 'first-item') {
@@ -13,3 +13,34 @@ function showPanels(p) {
     hiddenPanel[1].classList.toggle('active');
   }
 }
+
+// Consulta API Binance
+
+const btcPrice = 'https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT';
+const btcPriceVariation = 'https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT';
+const ethPrice = 'https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT';
+const ethPriceVariation = 'https://api.binance.com/api/v3/ticker/24hr?symbol=ETHUSDT'
+
+var bitcoinPrice = document.getElementById('bitcoinPrice');
+
+fetch(btcPrice)
+.then((resp) => resp.json())
+.then(function(data) {
+  let precioSinDecimales = Math.floor(data.price);
+  bitcoinPrice.textContent = `${precioSinDecimales} USD`;
+  console.log(data.price);
+})
+.catch(function(error) {
+  bitcoinPrice.textContent = "Precio No Disp";
+})
+
+fetch(btcPriceVariation)
+.then((resp) => resp.json())
+.then(function(data) {
+  if (data.priceChangePercent.includes('-')) {
+    console.log('el precio de BTC bajó')
+  }
+  else {
+    console.log('el precio de BTC subió')
+  }
+})
